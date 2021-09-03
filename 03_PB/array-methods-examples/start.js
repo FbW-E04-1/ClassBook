dbc7b1705e99b721b570225e697c550877c70eb7
Carlo <26264+carlotrimarchi@users.noreply.github.com>
2021-09-03 10:58:04 +0200

    Create start.js

---
+++ 03_PB/array-methods-examples/start.js
@@ -0,0 +1,88 @@
+let products = [
+  {
+    type: "t-shirt",
+    price: 9.99,
+    color: "red",
+  },
+  {
+    type: "socks",
+    price: 4.99,
+    color: "blue",
+  },
+  {
+    type: "jeans",
+    price: 44.99,
+    color: "black",
+  },
+  {
+    type: "t-shirt",
+    price: 14.99,
+    color: "green",
+  },
+  {
+    type: "t-shirt",
+    price: 22.99,
+    color: "blue",
+  },
+  {
+    type: "jeans",
+    price: 59.99,
+    color: "blue",
+  },
+  {
+    type: "skirt",
+    price: 24.99,
+    color: "yellow",
+  },
+  {
+    type: "socks",
+    price: 14.99,
+    color: "yellow",
+  },
+];
+
+/* Map */
+
+// Capitalize the type of each product
+
+// Get an array containing only product types.
+// Example: ["T-shirt", "Socks", "Jeans", "T-shirt"]
+
+// Add a new property to the products describing the product
+
+/* Filter and find */
+// Get an array with only yellow products
+
+// Get an array with products that cost more than 15
+
+// Get an array with listing only the types plus color of products that cost more than 15
+
+
+// Get the first jeans in the products
+
+
+// Check if there is at least a blue product
+
+// Check if there is at least a pink product
+
+
+// Check if there are at least 2 blue products
+
+
+/* Reduce */
+
+// Calculate the total price of products
+
+// Get an array with only product types, but without repetitions
+
+// Get the most expensive product
+
+
+
+
+
+// Get the cheapest product
+
+// Get the type and color of the cheapest product
+
+
