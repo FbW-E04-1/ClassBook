db595e21b99f29590907fb30de2acb6de5f1d4ce
Thomas Hofmann <thomas@savoy-truffle.de>
2021-11-25 11:40:33 +0100

    added argv.slice(2)

--- 05_BE/2021-11-24-server/live/tom/10-http-server/commandline-parameters.js
+++ 05_BE/2021-11-24-server/live/tom/10-http-server/commandline-parameters.js
@@ -1,8 +1,10 @@
 
-// for (let i=2; i<process.argv.length; i++) {
+// for (let i=0; i<process.argv.length; i++) {
 // 	console.log(i, process.argv[i]);
 // }
 
 let firstname = process.argv[2];
-
 console.log('hello', firstname);
+
+// var args = process.argv.slice(2); // cut out the first two
+// console.log(args);
