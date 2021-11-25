63e4d7203cf4267854d3a6c02a0cada5d372fe62
Thomas Hofmann <thomas@savoy-truffle.de>
2021-11-25 11:12:55 +0100

    disection of http server

---
+++ 05_BE/2021-11-24-server/live/tom/10-http-server/server-recap.js
@@ -0,0 +1,15 @@
+var http = require('http');
+// ! change the way modules are imported.
+
+const server = http.createServer(requestHandler);
+server.listen(8080);
+
+function requestHandler(req, res) {
+  console.log("request received");
+
+  // connect to a database... retrieve data and respond with
+
+  res.writeHead(200, {'Content-Type': 'text/plain'}); // write to HTTP header
+  res.write('Hello World!'); // write to HTTP body
+  res.end(); // end of message
+}
