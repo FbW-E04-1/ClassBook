1c75fe4e1ae853afc435b57147b406168e36317e
Dominik Brüchler <dominik@bruechler.de>
2022-01-10 12:53:45 +0100

    close connection in seed script to avoid having to call process.exit(0)

--- 05_BE/2022-01-05-faker/10-faker/lib/database.js
+++ 05_BE/2022-01-05-faker/10-faker/lib/database.js
@@ -4,17 +4,19 @@ dotenv.config();
 import mongodb from 'mongodb';
 const { MongoClient } = mongodb;
 
-const client = await MongoClient.connect(
-    process.env.MONGODB_URI, 
-    { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true 
+// to close the connection from seed.js, we want to export the client
+// in seed.js we can then call client.close()
+export const client = await MongoClient.connect(
+    process.env.MONGODB_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     }
 );
 
 // database is an optional parameter
 const database = 'online_magazine'
-const db = client.db(database);
+export const db = client.db();
 //const db = client.db();
 
-export default db;
+// since we already have two named exports we do not need to provide an additional default export
