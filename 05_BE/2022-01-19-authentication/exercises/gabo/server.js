a6d1073e8fa6d0edd113718cc550aa5684527088
Gabo984 <fernandezmendozagabriel1@gmail.com>
2022-01-20 16:16:01 +0100

    User signUp

---
+++ 05_BE/2022-01-19-authentication/exercises/gabo/server.js
@@ -0,0 +1,74 @@
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const express = require("express");
+const server = express();
+const CryptoJS = require("crypto-js");
+const mongoose = require("mongoose");
+const User = require("./models/UsersSchema");
+
+server.use(express.json()); //translate from json to javaScript
+
+mongoose.connect(
+  process.env.MONGO_URI,
+
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (!err) {
+      console.log("connected to database");
+    } else {
+      console.log(`error connected to the database ${err} `);
+    }
+  }
+);
+
+server.listen(process.env.PORT, () => {
+  console.log("connected with port " + process.env.PORT);
+});
+//usernameParam = process.argv[2];
+//passwordParam = process.argv[3];
+
+/* const USERS = {
+  alice: {
+    email: 'alice@gmail.com',
+    password:"1234"
+      //"$2b$12$zol5VgIoPLy.ogxswBKGM.486NcutM67czNoiJHnPDCLsatQqcfH.",
+  },
+  bob: {
+    email: "bob@example.com",
+    password:"2222"
+      //"$2b$12$BVNNzeCsqV.PGWhC7M.7GOMQmfFN2CqNeetERGzXAJ6U0/N/r41iq",
+  },
+}; */
+
+server.post("/signup", function (req, res) {
+  let email = req.body.email;
+  let password = req.body.password;
+  let saltRounds = 10;
+  let hash = bcrypt.hashSync(password, saltRounds);
+
+  try {
+    const user = new User({ email, password: hash });
+    user.save();
+    res.json(`user created successfully`);
+  } catch (error) {
+    console.log(`Could not creat a new user ${error}`);
+    res.status(400).json(`Could not creat a new user ${error}`);
+  }
+});
+
+// function authenticate(username, password) {
+//   // var passwordHash = calculateHash(username, password);
+//   // console.log(passwordHash);
+//   // return;
+
+//   if (!USERS[username]) {
+//     console.log("not authenticated");
+//     return;
+//   }
+//   if (bcrypt.hashSync(password, USERS[username].password)) {
+//     console.log("authenticated");
+//   } else {
+//     console.log("not authenticated");
+//   }
+// }
+// authenticate(usernameParam, passwordParam);
