1b58413b67240725d3f4b66af78cb2ffdb0a1fe8
Hiwa Taheri <81753997+hiwataheri@users.noreply.github.com>
2022-01-25 16:12:41 +0100

    token

---
+++ 05_BE/2022-01-19-authentication/exercises/hiwa/2022-01-25-jwt/example/test.js
@@ -0,0 +1,17 @@
+const { signToken, verifyToken } = require("./lib/token");
+
+// this is the data we put into the token
+// do not store any sensitive information like credentials in here as the client can easily access the data
+const payload = {
+    userId: 1,
+    name: "Dominik",
+    permissions: ["read_orders", "write_products"],
+};
+
+// when we use our custom token library, we do not need to take care of the default options or the secret
+// the whole token logic is encapsulated in lib/token.js
+const token = signToken(payload);
+
+const decodedPayload = verifyToken(token);
+console.log(decodedPayload);
+console.log(token);
