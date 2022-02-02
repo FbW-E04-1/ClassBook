835e4566434589cec8d1ea7dae01304b8fbd44d5
Thomas Hofmann <thomas@savoy-truffle.de>
2022-02-02 11:54:10 +0100

    moved pseudo code

--- 05_BE/2022-02-02-email/10-sendgrid-commandline/sendgrid.js
+++ 05_BE/2022-02-02-email/10-sendgrid-commandline/sendgrid.js
@@ -4,13 +4,6 @@ require('dotenv').config();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-
-recipients = [
-	'email-address1',
-	'email-address2',
-	'email-address3',
-];
-
 const msg = {
   to: 'marlenwieduwilt@aol.com',
   from: 'thomas.hofmann@digitalcareerinstitute.org', 
@@ -19,11 +12,6 @@ const msg = {
   html: '<strong></strong>',
 }
 
-// for each recipient of recipients{
-// 	msg.to = recipient;
-// 	sgMail.send(msg)...
-// //}
-
 sgMail.send(msg)
   .then(() => {
     console.log('Email sent to', msg.to, " subject:", msg.subject);
@@ -31,3 +19,14 @@ sgMail.send(msg)
   .catch((error) => {
     console.error('error sending to', msg.to, " subject:", msg.subject, error)
   })
+
+// pseudo code for sending to multiple receipients
+// receipients = [
+// 	'email-address1',
+// 	'email-address2',
+// 	'email-address3',
+// ];
+// for each recipient of receipients{
+// 	msg.to = recipient;
+// 	sgMail.send(msg)...
+// //}
