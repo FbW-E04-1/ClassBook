5a6f16c4eb330eb56ec5f78c198a126428600ace
tareq-almasri <talmasreq@gmail.com>
2022-02-08 15:44:05 +0100

    express-fileupload

---
+++ 05_BE/2022-02-08-uploading-files-with-express-upload/example/backend/middlewares/fileUpload.js
@@ -0,0 +1,26 @@
+const Path = require("path");
+
+const upload = function (req, res, next) {
+  let sampleFile;
+  let uploadPath;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.files.sampleFile;
+  uploadPath = Path.join(__dirname, "../public", sampleFile.name);
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath, function (err) {
+    if (err) return res.status(500).send(err);
+
+    res.filePath = uploadPath;
+
+    next();
+  });
+};
+
+
+module.exports = upload;
---
+++ 05_BE/2022-02-08-uploading-files-with-express-upload/example/backend/server.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+
+const express = require("express");
+const server = express();
+
+const fileUpload = require('express-fileupload');
+
+// default options
+server.use(fileUpload());
+
+require("./lib/database.js");
+
+const checkAuth = require("./middlewares/checkAuth.js");
+
+server.listen(process.env.PORT, () =>
+  console.log(`server listening on port ${process.env.PORT}`)
+);
+
+server.use(require("cookie-parser")());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+
+server.use("/api/auth", require("./routes/auth.js"));
+
+
+server.use("/api/photos", 
+//checkAuth,
+ require("./routes/photos.js"));
+
+
+server.use(
+  "/api/photographers",
+  checkAuth,
+  require("./routes/photographers.js")
+);
+
+// We can serve static files by using express.static.
+// All the files that are in the directory "./public" can be accessed via GET /files/<path to file>
+// whereas the path to file starts in "./public".
+// Example: photo3.jpg is located in ./public/subfolder
+// Our request needs to look like that: GET /files/subfolder/photo3.jpg
+server.use("/files", express.static("./public"));
+
+// We can serve our built frontend via express with express.static.
+server.use("/app", express.static("./app"));
+// By adding the following line, we make sure that whenever we try to access a subpage of our frontend directly
+// we serve index.html instead, which then handles the request with react router.
+server.use("/app/*", (req, res) => res.sendFile(__dirname + "/app/index.html"));
+
+server.use(require("./middlewares/handleError.js"));
+server.use("/", require("./middlewares/handleNotFound.js"));
